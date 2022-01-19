a71fe95273bd887f6d66ec006ba879fc51db01ec
Unknown <danila.vpopov@gmail.com>
2022-01-18 19:29:10 -0800

    breaking down the list of questions

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ document.getElementById("p1").innerHTML =
   "</span> scored <span>" +
   window.localStorage.getItem("Score") +
   "</span>";
+
+  function showQuetions(index) {
+    const titles = document.querySelector(".titles");
+  
+    let writeQuestions =
+      "<span>" + questions[index].qc + ". " + questions[index].title + "</span>";
+    let selectChoices =
+      '<div class="option"><span>' +
+      questions[index].choices[0] +
+      "</span></div>" +
+      '<div class="option"><span>' +
+      questions[index].choices[1] +
+      "</span></div>" +
+      '<div class="option"><span>' +
+      questions[index].choices[2] +
+      "</span></div>" +
+      '<div class="option"><span>' +
+      questions[index].choices[3] +
+      "</span></div>";
+    titles.innerHTML = writeQuestions;
+    qChoices.innerHTML = selectChoices;
+  
+    const option = qChoices.querySelectorAll(".option");
+  
+    for (i = 0; i < option.length; i++) {
+      option[i].setAttribute("onclick", "optionSelected(this)");
+    }
