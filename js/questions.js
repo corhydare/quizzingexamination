2fd89d0ffcaf7a6f8ea4f3ad44e535051fc890e5
Unknown <danila.vpopov@gmail.com>
2022-01-18 17:42:38 -0800

    adding questions

--- js/questions.js
+++ js/questions.js
@@ -1,15 +1,18 @@
-var questions = [
+let questions = [
   {
+    qc: 1,
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts",
   },
   {
+    qc: 2,
     title: "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
     answer: "parentheses",
   },
   {
+    qc: 3,
     title: "Arrays in JavaScript can be used to store ____.",
     choices: [
       "numbers and strings",
@@ -20,15 +23,23 @@ var questions = [
     answer: "all of the above",
   },
   {
+    qc: 4,
     title:
       "String values must be enclosed within ____ when being assigned to variables.",
     choices: ["commas", "curly brackets", "quotes", "parentheses"],
     answer: "quotes",
   },
   {
+    qc: 5,
     title:
       "A very useful tool used during development and debugging for printing content to the debugger is:",
     choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
     answer: "console.log",
+  },
+  {
+    qc: 6,
+    title: "A JavaScript program is a list of programming ___:",
+    choices: ["statements", "programs", "loops", "colors"],
+    answer: "statements",
   },
 ];
