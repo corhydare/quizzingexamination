9d4211f05a2cf662c847ea41e8b14d0dbdb5ab2d
Unknown <danila.vpopov@gmail.com>
2022-01-18 19:29:49 -0800

    didnt close

--- js/script.js
+++ js/script.js
@@ -6,29 +6,30 @@ document.getElementById("p1").innerHTML =
   window.localStorage.getItem("Score") +
   "</span>";
 
-  function showQuetions(index) {
-    const titles = document.querySelector(".titles");
-  
-    let writeQuestions =
-      "<span>" + questions[index].qc + ". " + questions[index].title + "</span>";
-    let selectChoices =
-      '<div class="option"><span>' +
-      questions[index].choices[0] +
-      "</span></div>" +
-      '<div class="option"><span>' +
-      questions[index].choices[1] +
-      "</span></div>" +
-      '<div class="option"><span>' +
-      questions[index].choices[2] +
-      "</span></div>" +
-      '<div class="option"><span>' +
-      questions[index].choices[3] +
-      "</span></div>";
-    titles.innerHTML = writeQuestions;
-    qChoices.innerHTML = selectChoices;
-  
-    const option = qChoices.querySelectorAll(".option");
-  
-    for (i = 0; i < option.length; i++) {
-      option[i].setAttribute("onclick", "optionSelected(this)");
-    }
+function showQuetions(index) {
+  const titles = document.querySelector(".titles");
+
+  let writeQuestions =
+    "<span>" + questions[index].qc + ". " + questions[index].title + "</span>";
+  let selectChoices =
+    '<div class="option"><span>' +
+    questions[index].choices[0] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    questions[index].choices[1] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    questions[index].choices[2] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    questions[index].choices[3] +
+    "</span></div>";
+  titles.innerHTML = writeQuestions;
+  qChoices.innerHTML = selectChoices;
+
+  const option = qChoices.querySelectorAll(".option");
+
+  for (i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
